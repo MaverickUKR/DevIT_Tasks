9dea37aeada5a7c400aece09708029bde54e281e
volodymyr_mazepa <mazepav88@gmail.com>
2024-07-31 12:10:18 +0300

    Deleted: 30-07

--- Tasks_DevIT/25-07/ecom-cms-test/source/app/.server/admin/loaders/auth.login.loader.ts
+++ Tasks_DevIT/25-07/ecom-cms-test/source/app/.server/admin/loaders/auth.login.loader.ts
@@ -1,21 +1,9 @@
-import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { LoaderFunctionArgs } from "@remix-run/node";
 import { authenticator } from "../services/auth.service";
-import { getSession, commitSession } from "../utils/session.util";
 import { EAdminNavigation } from "../constants/navigation.constant";
 
 export async function adminAuthLoader({ request }: LoaderFunctionArgs) {
-  await authenticator.isAuthenticated(request, {
+  return await authenticator.isAuthenticated(request, {
     successRedirect: EAdminNavigation.dashboard,
   });
-
-  const session = await getSession(request.headers.get("cookie"));
-  const error = session.get(authenticator.sessionErrorKey);
-  return json<{ error?: { message: string } }>(
-    { error },
-    {
-      headers: {
-        "Set-Cookie": await commitSession(session),
-      },
-    }
-  );
 }
